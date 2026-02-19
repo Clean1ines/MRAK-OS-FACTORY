92be876fc810d4ea3e719319384b3519872f6ddb
clean1ines <gurovsky90@gmail.com>
2026-02-19 21:32:35 +0300

    fix: add project root to sys.path to allow app module imports

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(__file__))
+
 from fastapi import FastAPI, Request
 from fastapi.responses import StreamingResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles

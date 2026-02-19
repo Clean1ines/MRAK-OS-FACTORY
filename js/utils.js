363048b776638058a949d2cceb13fcb0278190eb
clean1ines <gurovsky90@gmail.com>
2026-02-19 21:19:36 +0300

    refactor(frontend): split handlers.js into modules

---
+++ js/utils.js
@@ -0,0 +1,16 @@
+// utils.js - общие утилиты
+
+// Флаг для блокировки повторных сохранений (глобальный)
+window.isSaving = false;
+
+window.setLoading = function(button, isLoading) {
+    if (!button) return;
+    if (isLoading) {
+        button.disabled = true;
+        button.dataset.originalText = button.innerText;
+        button.innerHTML = '<span class="spinner"></span> Загрузка...';
+    } else {
+        button.disabled = false;
+        button.innerText = button.dataset.originalText || button.innerText;
+    }
+};
